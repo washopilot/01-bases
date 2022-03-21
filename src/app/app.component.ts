ce5bca8164f08136761af46033eff640b5957b90
Fernando Chicaiza <washopilot@yahoo.com>
2022-03-21 10:14:46 -0500

    tarea base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  title = 'bases';
+  titulo: string = 'Contador App';
+  numero: number = 10;
+  base: number = 5;
+
+  acumular(valor: number) {
+    this.numero += valor;
+  }
 }
