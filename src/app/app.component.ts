695e1ab903dd2e171cfdf67765a8d3b2244d38d1
Fernando Chicaiza <washopilot@yahoo.com>
2022-03-21 11:47:15 -0500

    crear componente de forma manual

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  titulo: string = 'Contador App';
-  numero: number = 10;
-  base: number = 5;
 
-  acumular(valor: number) {
-    this.numero += valor;
-  }
 }
