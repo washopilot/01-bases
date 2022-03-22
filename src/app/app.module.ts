76dbc274b0304819147ed5260b7c42c66aadaaf2
Fernando Chicaiza <washopilot@yahoo.com>
2022-03-22 08:53:17 -0500

    Módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { ListadoComponent } from './heroes/listado/listado.component';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
-  declarations: [AppComponent, ContadorComponent, HeroeComponent, ListadoComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, ContadorComponent],
+  imports: [BrowserModule, HeroesModule],
   providers: [],
   bootstrap: [AppComponent]
 })
