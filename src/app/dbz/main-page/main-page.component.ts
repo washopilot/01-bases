3551fd10fe5534857bbf687a5a39e2690b3fc857
Fernando Chicaiza <washopilot@yahoo.com>
2022-03-22 11:10:38 -0500

    two way data biding [(ngModel)]

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
+  nuevo: Personaje = {
+    nombre: 'Trunks',
+    poder: 14000
+  };
+
   agregar(): void {
-    console.log('Este es una prueba');
+    console.log(this.nuevo);
+  }
+
+  cambiarNombre() {
+    console.log();
   }
 }
